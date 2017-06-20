d7987326be3670d0ce3d2899d3b06bf5fd7b5aee
Ian Deans <ideans715@gmail.com>
2017-06-19 18:31:51 -0700

    Add authentication using passport.js

--- server.js
+++ server.js
@@ -1,34 +1,62 @@
 const express = require('express')
+const session = require('express-session')
+const flash = require('connect-flash')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt-nodejs')
 const database = require('./database')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const app = express()
+
+const routes = require('./routes')
+
+app.use(session({
+  secret: 'steez',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 60000}
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('ejs')
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (request, response) => {
-  database.getAlbums((error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      response.render('index', { albums: albums })
-    }
-  })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(flash())
+app.use(function( request, response, next) {
+  response.locals.messages = require('express-messages')(request, response)
+  next()
 })
 
-app.get('/albums/:albumID', (request, response) => {
-  const albumID = request.params.albumID
+app.use(routes)
 
-  database.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      const album = albums[0]
-      response.render('album', { album: album })
-    }
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},  
+  function(email, password, done) {
+    database.User.findByEmail(email, (error, user) => {
+      user = user[0]
+      if (error) { return done(error) }
+      if (!user) { return done(null, false, {message: 'Email not found.'})}
+      if ( !bcrypt.compareSync(password, user.password) ) {
+        return done(null, false, {message: 'Invalid password.'})
+      }
+      return done(null, user)
+    })
+  }
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+passport.deserializeUser(function(id, done) {
+  database.User.findById(id, function(error, user) {
+    done(null, user)
   })
 })
 
